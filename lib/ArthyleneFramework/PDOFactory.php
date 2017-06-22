80035c6a243145ed2cdf3a2ace6bc28cae8bb020
Thibault <thibault.nougues@hotmail.fr>
2017-06-22 15:52:04 +0200

    ARTHY-26 #comment test comment 4

--- lib/ArthyleneFramework/PDOFactory.php
+++ lib/ArthyleneFramework/PDOFactory.php
@@ -15,7 +15,6 @@ class PDOFactory
     {
         $db = new \PDO('mysql:host=localhost;dbname=arthylene', 'root', 'root');
         $db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-
         return $db;
     }
 }
